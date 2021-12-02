24d66e1b8058e8ba75016bf1f47f974e68582f68
cosimop2000 <pavone.cosimo2000@gmail.com>
2021-12-02 15:44:27 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+def print_hi(name):
+
+    print(f'Hi, {name}')
+
+
+def print_prova():
+
+    print("Iron Maiden" + ' : Run to the hills')
+
+
+if __name__ == '__main__':
+    print_hi('Cosimo')
+    print_prova()
+    print('Hello world')
